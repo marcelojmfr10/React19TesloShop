5b5ec753bb0e04076a66ed4b583d50f1e04542ea
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-05-15 21:20:44 -0600

    Estructura básica de proyecto

---
+++ src/TesloShopApp.tsx
@@ -0,0 +1,7 @@
+export const TesloShopApp = () => {
+  return (
+    <div>
+      <h1>teslo</h1>
+    </div>
+  );
+};
---
+++ src/admin/pages/product/AdminProductPage.tsx
@@ -0,0 +1,3 @@
+export const AdminProductPage = () => {
+  return <div></div>;
+};
